d0017c946c902a8361d508c7d75c23effe7f5e81
ranvane <ranvane@gmail.com>
2013-01-05 21:19:24 +0800

    2

---
+++ sono/tool/downPage.py
@@ -0,0 +1,19 @@
+# -*- encoding:utf-8 -*-
+'''
+Created on 2012-12-20
+
+@author: ranvane
+
+www.ranblog.info
+'''
+class downPage(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        pass
